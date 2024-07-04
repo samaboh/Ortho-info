764f4ec163f12629be6d2d33d10212dbb6ade72b
samaboh <abohsamson665@gmail.com>
2024-07-04 11:36:54 +0100

    x

--- README.md
+++
@@ -1,2 +0,0 @@
-# www
-Ortho Info Public portal
--- src/Site.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Site from './components/Site';
-
-test('renders learn react link', () => {
-	render(<Site />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
-});
